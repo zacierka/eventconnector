8361cec21d423839180f5090a3f7aa4e725fb750
Mike <mczacierka@gmail.com>
2022-07-08 18:14:10 -0400

    working

--- eventconnector/eventconnector.cpp
+++ eventconnector/eventconnector.cpp
@@ -15,53 +15,179 @@ void eventconnector::onLoad()
 	_globalCvarManager = cvarManager;
 
 	cvarManager->log("Plugin loaded!");
-	enabled = std::make_shared<bool>(false);
+	enabled = std::make_shared<bool>(true);
 
-	registerHookEvents();
 	registerCvars();
+	HookAllEvents();
 }
 
 void eventconnector::onUnload()
 {
-	gameWrapper->UnhookEvent("Function TAGame.Ball_TA.Explode");
+	gameWrapper->UnhookEvent(EVENT_HOOK_GOAL);
+	//gameWrapper->UnhookEvent(EVENT_HOOK_MATCHSTART);
 
-	_globalCvarManager->removeCvar("eventconnector_enabled");
-	_globalCvarManager->removeCvar("eventconnector_disabled");
-	_globalCvarManager->removeCvar("eventconnector_setURL");
-	_globalCvarManager->removeCvar("eventconnector_setEndpoint");
+	_globalCvarManager->removeCvar("EC_Enable");
+	_globalCvarManager->removeCvar("EC_SetEndpoint");
 
 	cvarManager->log("Plugin Unloaded!");
 }
 
-void eventconnector::registerHookEvents()
-{
-	gameWrapper->HookEvent("Function TAGame.Ball_TA.Explode", std::bind(&eventconnector::onExplode, this));
-}
-
 void eventconnector::registerCvars()
 {
-	_globalCvarManager->registerCvar("eventconnector_enabled", "1", "Enable eventconnector", true, true, 0, true, 1);
-	_globalCvarManager->registerCvar("eventconnector_disabled", "0", "Disable eventconnector");
+	try {
+		cvarManager->registerCvar("EC_Enable", "1", "Event NodeRed Connector", true, true, 0, true, 1).bindTo(enabled);
 
-	_globalCvarManager->registerCvar("eventconnector_setURL", "127.0.0.1", "URL for NodeRed Endpoint", true, true, 0, true, 1)
-		.addOnValueChanged([this](std::string oldValue, CVarWrapper cvar) {
+		_globalCvarManager->registerCvar("EC_SetEndpoint", "192.168.1.29", "URL for NodeRed Endpoint", true, true, 0, true, 1)
+			.addOnValueChanged([this](std::string oldValue, CVarWrapper cvar) {
 			NODERED_URL = cvar.getStringValue();
 		});
-
-	_globalCvarManager->registerCvar("eventconnector_setEndpoint", "wled", "NodeRed Endpoint", true, true, 0, true, 1)
-		.addOnValueChanged([this](std::string oldValue, CVarWrapper cvar) {
-			NODERED_ENDPOINT = cvar.getStringValue();
-		});
+	}
+	catch (std::exception& e)
+	{
+		LOG("Plugin failed to load! Please contact plugin developer");
+		LOG("{}:{}", __FUNCTION__, e.what());
+	}
 }
 
-void eventconnector::onExplode()
+void eventconnector::sendGoalEvent(int teamColor)
 {
-	// Threaded to ignore the blocking call of the Get request
-	std::thread t([this]() {
-		std::string url = std::string(NODERED_URL + " " + NODERED_ENDPOINT);
+	// 0 = Blue, 1 = Orange
+	std::string team = teamColor == 0 ? "blue" : "orange";
+	httplib::Params params;
+	params.emplace("event", "goal");
+	params.emplace("team", team);
+
+	sendEvent(params);
+}
+
+void eventconnector::sendMatchStartEvent(int teamColor)
+{
+	std::string team = teamColor == 0 ? "blue" : "orange";
+	httplib::Params params;
+	params.emplace("event", "matchstart");
+	params.emplace("team", team);
+
+	sendEvent(params);
+}
+
+
+void eventconnector::sendEvent(httplib::Params inParams)
+{
+	std::thread t([this](httplib::Params params, std::string inUrl, int port) {
+		std::string url = std::string(inUrl + ":" + std::to_string(port));
 		httplib::Client cli(url.c_str());
 
-		cli.Get("/goalevent"); // Ignore resulting status code
-	});
+		cli.Post("/rleventconnector", params); // Ignore resulting status code
+		}, inParams, NODERED_URL, NODERED_PORT);
+
 	t.detach();
+}
+
+
+ServerWrapper eventconnector::GetCurrentGameState(std::shared_ptr<GameWrapper> gameWrapper)
+{
+	if (gameWrapper->IsInReplay())
+		return gameWrapper->GetGameEventAsReplay().memory_address;
+	else if (gameWrapper->IsInOnlineGame())
+		return gameWrapper->GetOnlineGame();
+	else
+		return gameWrapper->GetGameEventAsServer();
+}
+
+bool eventconnector::ShouldRun(std::shared_ptr<GameWrapper> gameWrapper)
+{
+	//Check if server exists
+	ServerWrapper server = GetCurrentGameState(gameWrapper);
+	if (server.IsNull())
+	{
+		LOG("server.IsNull(): (need false) true");
+		return false;
+	}
+
+	//DisAllow in replay mode
+	if (gameWrapper->IsInReplay())
+	{
+		return false;
+	}
+
+	if (gameWrapper->IsInFreeplay())
+	{
+		return false;
+	}
+
+	//Check if player is spectating
+	if (!gameWrapper->GetLocalCar().IsNull())
+	{
+		LOG("GetLocalCar().IsNull(): (need true) false");
+		return false;
+	}
+
+	return true;
+}
+
+
+void eventconnector::HookAllEvents()
+{
+	using namespace std::placeholders;
+
+	//GAME EVENTS
+	// gameWrapper->HookEventPost("Function TAGame.Team_TA.PostBeginPlay", std::bind(&eventconnector::HookInitTeams, this));
+	// gameWrapper->HookEventPost("Function TAGame.GameEvent_Soccar_TA.Destroyed", std::bind(&eventconnector::HookMatchDestroyed, this));
+	// gameWrapper->HookEventPost("Function TAGame.GameEvent_Soccar_TA.EventMatchEnded", std::bind(&eventconnector::HookMatchEnded, this));
+	gameWrapper->HookEventWithCallerPost<ActorWrapper>("Function TAGame.ReplayDirector_TA.OnScoreDataChanged", std::bind(&eventconnector::HookReplayScoreDataChanged, this, _1));
+}
+
+
+void eventconnector::HookInitTeams()
+{
+	static int NumTimesCalled = 0;
+
+	//"Function TAGame.Team_TA.PostBeginPlay" is called twice rapidly, once for each team
+	// Only initialize lobby on the second hook once both teams are ready
+
+	++NumTimesCalled;
+	if (NumTimesCalled >= 2)
+	{
+		//Set a delay so that everything can be filled in before trying to initialize
+		gameWrapper->SetTimeout([this](GameWrapper* gw)
+			{
+				if (ShouldRun(gameWrapper))
+				{
+					// Do something
+				}
+			}, .05f);
+
+		NumTimesCalled = 0;
+	}
+
+	//Reset call counter after 2 seconds in case it never got through the >= 2 check
+	if (NumTimesCalled != 0)
+	{
+		gameWrapper->SetTimeout([this](GameWrapper* gw) { NumTimesCalled = 0; }, 2.f);
+	}
+
+}
+
+void eventconnector::HookReplayScoreDataChanged(ActorWrapper caller)
+{
+	ServerWrapper server = gameWrapper->GetCurrentGameState();
+	if (server.IsNull()) { LOG("SERVER IS BAD"); return; }
+
+	ReplayDirectorWrapper RDW(caller.memory_address);
+	if (RDW.IsNull()) { return; }
+	ReplayScoreData scoreData = RDW.GetReplayScoreData();
+	if (caller.IsNull()) { LOG("CALLER IS BAD"); return; }
+	LOG("CALLER IS GOOD");
+	
+	LOG("SERVER IS GOOD");
+	auto primary = server.GetLocalPrimaryPlayer();
+	if (!primary) { LOG("PRI IS BAD"); return; }
+	LOG("PLAYER IS GOOD");
+	LOG("CALLER IS GOOD {}", caller.GetTeamNum2());
+	LOG("LOCAL IS GOOD {}", primary.GetTeamNum2());
+	if (scoreData.ScoreTeam == primary.GetTeamNum2())
+	{
+		LOG("GOAL SCORED BY PLAYER TEAM");
+		sendGoalEvent(primary.GetTeamNum2());
+	}
 }
--- eventconnector/eventconnector.h
+++ eventconnector/eventconnector.h
@@ -5,33 +5,44 @@
 #include "bakkesmod/plugin/PluginSettingsWindow.h"
 #include "version.h"
 #include "httplib.h"
-constexpr auto plugin_version = stringify(VERSION_MAJOR) "." stringify(VERSION_MINOR) "." stringify(VERSION_PATCH) "." stringify(VERSION_BUILD);
 
+constexpr auto plugin_version = stringify(VERSION_MAJOR) "." stringify(VERSION_MINOR) "." stringify(VERSION_PATCH) "." stringify(VERSION_BUILD);
 
 class eventconnector: public BakkesMod::Plugin::BakkesModPlugin/*, public BakkesMod::Plugin::PluginSettingsWindow*//*, public BakkesMod::Plugin::PluginWindow*/
 {
 public:
-
-	std::shared_ptr<bool> enabled;
-
-	// Node-Red Setup
-	std::string NODERED_URL = "127.0.0.1";
-	std::string NODERED_ENDPOINT = "rocketleague";
-
-	std::string HOOK_LEAVE_GAME = "Function TAGame.GameEvent_Soccar_TA.Destroyed";
-	std::string HOOK_PODIUM_SHOW = "Function TAGame.GameEvent_Soccar_TA.EventMatchEnded";
-	std::string HOOK_START_GAME = "Function GameEvent_Soccar_TA.WaitingForPlayers.BeginState";
-	std::string HOOK_BEGIN_MATCH = "Function TAGame.Team_TA.PostBeginPlay";
-	//Boilerplate
-
-private:
 	virtual void onLoad();
 	virtual void onUnload();
+
+
+private:
 	// Registering Events
-	void registerHookEvents();
 	void registerCvars();
 	
-	// Game Events
-	void onExplode();
-};
+	// HOOKS (EventHooks.cpp)
+	void HookAllEvents();
+	void HookInitTeams();
+	// void HookMatchCreated();
+	// void HookMatchDestroyed();
+	// void HookMatchEnded();
+	void HookReplayScoreDataChanged(ActorWrapper caller);
 
+	ServerWrapper GetCurrentGameState(std::shared_ptr<GameWrapper> gameWrapper);
+	bool ShouldRun(std::shared_ptr<GameWrapper> gameWrapper);
+
+	void sendGoalEvent(int color);
+	void sendMatchStartEvent(int color);
+	void sendEvent(httplib::Params inParams);
+	// CVARS
+	std::shared_ptr<bool> enabled             = std::make_shared<bool>(false);
+	// IP for NodeRed Endpoint
+	std::string NODERED_URL = "192.168.1.29";
+	int NODERED_PORT = 1880;
+
+
+	// Game Events
+	const std::string EVENT_HOOK_GOAL         = "Function TAGame.ReplayDirector_TA.OnScoreDataChanged";
+	const std::string EVENT_HOOK_MATCHSTART   = "Function TAGame.Team_TA.PostBeginPlay";
+	const std::string EVENT_HOOK_MATCHDESTROY = "Function TAGame.GameEvent_Soccar_TA.Destroyed";
+	const std::string EVENT_HOOK_MATCHEND     = "Function TAGame.GameEvent_Soccar_TA.EventMatchEnded";
+};
